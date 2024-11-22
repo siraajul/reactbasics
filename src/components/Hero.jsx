55d1acb69f6dbee1b0ce160d57602a6df04bbc15
siraajul <shirajulislamparvez@gmail.com>
2024-11-22 20:45:40 +0600

    Update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Hero = () => {
   return (
     <div>
-      <h1>This is Hero Component</h1>
+      <img
+        src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
+        alt=""
+      />
     </div>
   );
 };
